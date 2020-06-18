37d81b577c5aa35de69a865fad701bceb94ffad2
André Heber <a.heber@sabienzia.com>
2020-06-18 22:27:23 +0200

    connected with repl.it

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello GitHub\n");
+int main ( int argc, char ** argv ) {
+    if ( argc != 2 ) {
+        printf ( "# main develop\n");
+        exit ( 0 );
+    }
+
+    if ( strcmp ( argv [ 1 ] , "develop" ) == 0 )
+        system("open https://repl.it/@AndreHeber/fastdev");
+
   return 0;
 }
