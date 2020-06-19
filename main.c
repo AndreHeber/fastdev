faa67a52db370d5c8baa117ff82eb81406c6ab59
André Heber <andre.heber@gmx.net>
2020-06-19 08:20:17 +0000

    moved to gitpod.io

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define WEBSITE_DEV https://gitpod.io/#https://github.com/AndreHeber/fastdev 
+
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
    //define something for Windows (32-bit and 64-bit, this part is common)
-   #define START_BROWSER "explorer https://repl.it/@AndreHeber/fastdev"
+   #define START_BROWSER "explorer WEBSITE_DEV"
    #ifdef _WIN64
       //define something for Windows (64-bit only)
    #else
@@ -18,26 +20,26 @@
         // iOS device
     #elif TARGET_OS_MAC
         // Other kinds of Mac OS
-		#define START_BROWSER "open https://repl.it/@AndreHeber/fastdev"
+		#define START_BROWSER "open WEBSITE_DEV"
     #else
     #   error "Unknown Apple platform"
     #endif
 #elif __linux__
     // linux
-	#define START_BROWSER "open https://repl.it/@AndreHeber/fastdev"
+	#define START_BROWSER "open WEBSITE_DEV"
 #elif __unix__ // all unices not caught above
     // Unix
-	#define START_BROWSER "open https://repl.it/@AndreHeber/fastdev"
+	#define START_BROWSER "open WEBSITE_DEV"
 #elif defined(_POSIX_VERSION)
     // POSIX
-	#define START_BROWSER "open https://repl.it/@AndreHeber/fastdev"
+	#define START_BROWSER "open WEBSITE_DEV"
 #else
 #   error "Unknown compiler"
 #endif
 
 int main ( int argc, char ** argv ) {
     if ( argc != 2 ) {
-        printf ( "# main develop\n");
+        printf ( "# %s develop\n", argv [ 0 ] );
         exit ( 0 );
     }
 
